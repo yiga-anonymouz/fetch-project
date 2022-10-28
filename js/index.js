6d7906ffbafa7f839c82f90e571e50142f48acf6
0xYiga <47826286+yiga-anonymouz@users.noreply.github.com>
2022-10-28 17:51:11 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ close.addEventListener('click', () => {
     header.classList.remove('show')
 })
 
-fetch('../fetch-project/faq.json')
+fetch('https://yiga-anonymouz.github.io/fetch-project/faq.json')
 .then(response => response.json())
 .then((data) => {
     let output = ``
