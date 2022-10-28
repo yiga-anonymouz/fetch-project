730a5553589a8e380e9438c23a07c8e1ea6a7a7b
0xYiga <47826286+yiga-anonymouz@users.noreply.github.com>
2022-10-28 18:15:58 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ close.addEventListener('click', () => {
     header.classList.remove('show')
 })
 
-fetch('https://yiga-anonymouz.github.io/fetch-project/faq.json')
+fetch('../faq.json')
 .then(response => response.json())
 .then((data) => {
     let output = ``
